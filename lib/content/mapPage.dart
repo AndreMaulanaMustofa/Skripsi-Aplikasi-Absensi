609abc4cebe4d40f20fa6e148efbdb6f36c334f4
AndreMaulanaMustofa <andremm73@gmail.com>
2024-02-21 16:37:33 +0700

    Menampilkan map pada aplikasi

--- lib/content/mapPage.dart
+++ lib/content/mapPage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: sort_child_properties_last, camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class mapPage extends StatefulWidget {
   const mapPage({super.key});
@@ -32,6 +34,33 @@ class _mapPageState extends State<mapPage> {
               color: Colors.green,
               height: 60,
             ),
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  initialCenter: LatLng(-7.94295, 112.63572),
+                  initialZoom: 19,
+                ), 
+                children: [
+                  TileLayer(
+                    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'com.example.app',
+                  ),
+                  MarkerLayer(
+                    markers: [
+                      Marker(
+                        point: LatLng(-7.94295, 112.63572),
+                        child: Builder(
+                          builder: (context) => Icon(
+                            Icons.pin_drop,
+                            color: Colors.red,
+                          )
+                        )
+                      )
+                    ]
+                  )
+                ]
+              )
+            )
           ],
         ),
       ),
