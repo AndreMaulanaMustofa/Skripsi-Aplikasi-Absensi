5db911313b048ff22e9823d9920540490e342254
AndreMaulanaMustofa <andremm73@gmail.com>
2024-04-01 14:33:22 +0700

    Pembuatan Scan page untuk membaca sebuah QR Code yang disediakan

--- lib/content/scanPage.dart
+++ lib/content/scanPage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class scanPage extends StatefulWidget {
   const scanPage({super.key});
@@ -8,13 +10,52 @@ class scanPage extends StatefulWidget {
 }
 
 class _scanPageState extends State<scanPage> {
+
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+  QRViewController ? controller;
+  Barcode? result;
+
+  @override
+  void dispose(){
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _onQRViewCreated(QRViewController controller){
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) { 
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Text('Percobaan Page'),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 5,
+                child: QRView(
+                  key: qrKey,
+                  onQRViewCreated: _onQRViewCreated,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: (result != null)
+                    ? Text(
+                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                    : Text('Scan a code'),
+                ),
+              )
+            ],
+          ) 
         ),
       ),
     );
