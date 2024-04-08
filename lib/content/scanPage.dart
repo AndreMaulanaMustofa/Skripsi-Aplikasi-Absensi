000aff1478e1aba5e71c35c80d16bdde137c5851
AndreMaulanaMustofa <andremm73@gmail.com>
2024-04-08 15:19:33 +0700

    Pemisahan antara jam dan matkul pada saat mahasiswa mendapat data dari qr code

--- lib/content/scanPage.dart
+++ lib/content/scanPage.dart
@@ -1,9 +1,19 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class scanPage extends StatefulWidget {
-  const scanPage({super.key});
+  final String NIM;
+  final String namaLengkap;
+  final String kelas;
+  final String semester;
+
+  const scanPage ({
+    super.key, 
+    required this.NIM, 
+    required this.namaLengkap, 
+    required this.kelas, 
+    required this.semester
+  });
 
   @override
   State<scanPage> createState() => _scanPageState();
@@ -14,6 +24,9 @@ class _scanPageState extends State<scanPage> {
   final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
   QRViewController ? controller;
   Barcode? result;
+  String matkul = '';
+  String jamMatkul = '';
+  bool isBottomSheetOpened = false;
 
   @override
   void dispose(){
@@ -26,8 +39,330 @@ class _scanPageState extends State<scanPage> {
     controller.scannedDataStream.listen((scanData) { 
       setState(() {
         result = scanData;
+        if(!isBottomSheetOpened){
+          if (result!.code != null && result!.code!.contains(',')) {
+              matkul = result!.code!.split(',')[0].trim();
+              jamMatkul = result!.code!.split(',')[1].trim();
+              isBottomSheetOpened = true;
+
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                isDismissible: true,
+                builder: (BuildContext context) {
+                  return Container(
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(right: 50, left: 50),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 31, 3, 155),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          alignment: Alignment.center,
+                          width: 100,
+                          height: 40,
+                          child: const Text(
+                            'Data Absensi',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Roboto',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 55),
+                          margin: const EdgeInsets.only(top: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: const Text(
+                            'NIM:',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(right: 65),
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: Text(
+                            '${nimController.text}',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 55),
+                          margin: const EdgeInsets.only(top: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: const Text(
+                            'Nama Mahasiswa:',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(right: 65),
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: Text(
+                            '${namaController.text}',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 55),
+                          margin: const EdgeInsets.only(top: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: const Text(
+                            'Kelas:',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(right: 65),
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: Text(
+                            '${kelasController.text}',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 55),
+                          margin: const EdgeInsets.only(top: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: const Text(
+                            'Semester:',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(right: 65),
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: Text(
+                            '${semesterController.text}',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 55),
+                          margin: const EdgeInsets.only(top: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: const Text(
+                            'Mata Kuliah:',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(right: 65),
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: Text(
+                            '${matkul}',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 55),
+                          margin: const EdgeInsets.only(top: 20),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: const Text(
+                            'Jam Matkul:',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          padding: const EdgeInsets.only(right: 65),
+                          margin: const EdgeInsets.only(top: 10, bottom: 30),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          width: 100,
+                          child: Text(
+                            '${jamMatkul}',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Roboto',
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green.shade400,
+                              ),
+                              child: const Row(
+                                children: [
+                                  Icon(
+                                    Icons.save_alt_sharp,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    'Absen',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onPressed: () {},
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.red.shade400,
+                              ),
+                              child: const Row(
+                                children: [
+                                  Icon(
+                                    Icons.cancel,
+                                    color: Colors.white,
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    'Cancel',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onPressed: () {
+                                Navigator.pop(context);
+                                isBottomSheetOpened = false;
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ));
+                });
+            }
+          };
       });
     });
+  }
+
+  TextEditingController nimController = TextEditingController();
+  TextEditingController namaController = TextEditingController();
+  TextEditingController semesterController = TextEditingController();
+  TextEditingController kelasController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nimController = TextEditingController(text: widget.NIM);
+    namaController = TextEditingController(text: widget.namaLengkap);
+    semesterController = TextEditingController(text: widget.semester);
+    kelasController = TextEditingController(text: widget.kelas);
   }
   
   @override
@@ -43,19 +378,19 @@ class _scanPageState extends State<scanPage> {
                 child: QRView(
                   key: qrKey,
                   onQRViewCreated: _onQRViewCreated,
+                  cameraFacing: CameraFacing.back,
+                  formatsAllowed: const [BarcodeFormat.qrcode],
+                  overlay: QrScannerOverlayShape(
+                    borderColor: Colors.blue,
+                    borderRadius: 10,
+                    borderLength: 30,
+                    borderWidth: 10,
+                    cutOutSize: MediaQuery.of(context).size.width * 0.8,
+                  ),
                 ),
               ),
-              Expanded(
-                flex: 1,
-                child: Center(
-                  child: (result != null)
-                    ? Text(
-                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-                    : Text('Scan a code'),
-                ),
-              )
             ],
-          ) 
+          ),
         ),
       ),
     );
